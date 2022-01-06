d6da59051404c4b65a4a06e9d79d96feb833281f
wesmith <smiwarsky@gmail.com>
2022-01-05 21:26:26 -0700

    corrected how absolute location of script was found, added full path to tpl file

--- python/rpi_status.py
+++ python/rpi_status.py
@@ -22,14 +22,25 @@ def get_port(argv=sys.argv):
     port = parser.parse_args(argv[1:]).PORT
     print('\nUsing PORT: {}'.format(port))
     return port
-
+'''
 def get_css_path(css_local_path='/static/css'):
-    dd = os.popen('pwd')
-    dirname  = dd.read()[:-1]
+    #dd = os.popen('pwd')
+    #dirname  = dd.read()[:-1]
+    dirname = os.path.dirname(os.path.realpath(__file__))
     css_path = dirname + css_local_path
-    print('\nRunning from: {}\n\nUsing CSS path: {}\n'.\
+    print('\nScript in: {}\n\nUsing CSS path: {}\n'.\
           format(dirname, css_path))
     return css_path
+'''
+def get_paths(tpl_local_path='/views',
+              css_local_path='/static/css'):
+    # get path to script, no matter where it is run from
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    tpl_path = dirname + tpl_local_path
+    css_path = dirname + css_local_path
+    print('\nScript in: {}\n\nUsing CSS path: {}\n'.\
+          format(dirname, css_path))
+    return tpl_path, css_path
 
 def get_temp():
     temp_C = vccmd.measure_temp()
@@ -96,21 +107,25 @@ def get_processes_in_text(num=5):
     return txt
 
 
-def run(argv, nproc=10, fields=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-        debug_on=True, css_local_path='/static/css'):
+def run(argv, nproc=10, 
+        fields=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        debug_on=True, 
+        tpl_local_path='/views',
+        css_local_path='/static/css'):
     '''
     nproc:  number of processes to display
     fields: 'ps aux' output fields to display (default: all fields)
             (e.g., for a subset: fields = [0, 1, 2, 3, 8, 9, 10])
     '''
-
     port = get_port(argv=argv)
 
     app = Bottle()
 
     debug(debug_on) # turn off in production env
 
-    css_path = get_css_path(css_local_path=css_local_path)
+    #css_path = get_css_path(css_local_path=css_local_path)
+    tpl_path, css_path = get_paths(tpl_local_path=tpl_local_path,
+                                   css_local_path=css_local_path)
     @app.route('/static/<filename:re:.*\.css>')
     def send_css(filename):
         return static_file(filename, root=css_path)
@@ -123,7 +138,7 @@ def run(argv, nproc=10, fields=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
     def index():
         dd1 = [k() for k in fns]
         dd2 = get_processes_in_list(num=nproc, fields=fields)
-        return template('views/rpi_status.tpl',
+        return template(os.path.join(tpl_path, 'rpi_status.tpl'),
                         css_file='rpi_status.css',
                         name1='Raspberry Pi 4 Status on Port {}'.\
                         format(port),
